0bf8096278e12a2e4a9f716d83bea73c3945cb0d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-21 06:10:15 -0400

    Update StarToken.sol

--- StarToken.sol
+++ StarToken.sol
@@ -1,14 +1,13 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 string constant name = "StarToken";
-
 string constant symbol = "STR";
 
-contract StarToken is ERC721(name, symbol) {
+contract StarToken is ERC721 {
 
         address public owner;
         uint256 tokenId = 1;
@@ -25,7 +24,7 @@ contract StarToken is ERC721(name, symbol) {
     mapping(address => Star[]) public tokenAddress;
     mapping(string => bool) public tokenExists;
     
-    constructor() ERC721(name, symbol){
+    constructor() ERC721("StarToken", "STR"){
         
         owner = msg.sender;
     }
