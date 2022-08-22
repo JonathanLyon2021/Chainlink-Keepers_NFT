e8f8378ac5f4bd4334f2f554b1c0350e4187d1a5
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-22 16:44:42 -0400

    Create AggregatorV3Interface.sol

---
+++ AggregatorV3Interface.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+abstract contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    
+}
