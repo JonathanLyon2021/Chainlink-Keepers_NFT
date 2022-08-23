a3b12cd352ddece95e813bdcd43bd460379bca82
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-22 22:34:50 -0400

    Update StarToken.sol

--- StarToken.sol
+++ StarToken.sol
@@ -41,7 +41,7 @@ contract StarToken is ERC721 {
     
     function mintToken(string calldata _tokenName) public payable{
         //check to see if token already exists on blockchain, if so send authentication error message.
-        require(!tokenExists[_tokenName], "Token already exists");
+        require(!tokenExists[_tokenName], "Token already exists!");
 
         _safeMint(msg.sender, tokenId);
 
